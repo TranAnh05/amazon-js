766d2f9f3380c54c132fcbb62834065d1c1bcf36
Tran Anh <anh900472@gmail.com>
2025-05-25 18:06:38 +0700

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -77,3 +77,16 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
     saveToStorage();
 }
+
+export function loadCart(func) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        console.log("Load cart");
+
+        func();
+    });
+
+    xhr.open("GET", "https://supersimplebackend.dev/cart");
+    xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,39 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 import "../data/cart-class.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve("value1");
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    }),
+]).then((values) => {
+    console.log(values);
+
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve();
+//     });
+// })
+//     .then(() => {
+//         return new Promise((resolve) => {
+//             loadCart(() => {
+//                 resolve();
+//             });
+//         });
+//     })
+//     .then(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
